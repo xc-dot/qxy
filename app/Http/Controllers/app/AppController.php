3d4d891a4d1af8a4e4ac51a361158ecc41252f74
liaozengfeng <9165492845@qq.com>
2020-01-13 16:51:52 +0800

    aaa

---
+++ app/Http/Controllers/app/AppController.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Http\Controllers\app;
+
+use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+use DB;
+class AppController extends Controller
+{
+        public function add(Request $request)
+        {
+          echo 111;
+        }
+}
